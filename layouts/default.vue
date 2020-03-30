4dd8e6e17d8081a3aac01620199bb670d8343ae3
Виталий Канев <1xamelion1@gmail.com>
2020-03-30 16:14:36 +0300

    Add vue

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app light>
+    <v-app-bar color="primary" :clipped-left="clipped" fixed app short>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-container>
+        <v-row>
+          <v-spacer />
+          <v-col cols="4">
+            <v-select
+              v-model="dimension"
+              :items="dimensions"
+              :menu-props="{ offsetY: true, contentClass: 'primary' }"
+              color="blue"
+              dense
+              hide-details
+              label="Dimensions"
+              outlined
+              prepend-inner-icon="mdi-view-dashboard"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dimension: '',
+      dimensions: [],
+      clipped: false,
+      fixed: false,
+      title: 'Obalor'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+     safasfasf
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
